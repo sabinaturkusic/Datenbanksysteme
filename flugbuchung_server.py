94615668cdf5c3d4a5129d497e38ffd726f0159b
Lisa Schneider <lisa.schneider@stud.fh-campuswien.ac.at>
2019-03-11 16:02:39 +0100

    Add localhost and html templates

--- flugbuchung_server.py
+++ flugbuchung_server.py
@@ -2,10 +2,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def main():
+@app.route("/signUp")
+def sign_up():
     return render_template("sign_up.html")
 
 
+@app.route("/logIn")
+def log_in():
+    return render_template("log_in.html")
+
+
+@app.route("/chooseDate")
+def choose_date():
+    return render_template("choose_date.html")
+
+
+@app.route("/bookFlight")
+def book_flight():
+    return render_template("book_flight.html")
+
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run()
+
