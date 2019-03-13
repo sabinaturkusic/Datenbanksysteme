8b6a2c477c5a159eabf8a6766aca11225eead080
Lisa Schneider <lisa.schneider@stud.fh-campuswien.ac.at>
2019-03-13 17:12:39 +0100

    Update Readme

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Starten des Servers:
 2. python3 flugbuchung_server.py
 3. http://localhost:5000/signUp (bzw http://localhost:5000/<Name des HTML in camelCase>) im Browser eingeben
 4. Jetzt wird eine ziemlich basic HTML Seite angezeigt
+
+@ Datenbank: 
+Jeder braucht eine lokale Kopie der gesamten Datenbank, da wir ja unseren Server nur lokal hosten. Daher bitte: 
+1. MySQL installieren (siehe link in MySQL_installation.txt)
+2. jQuery installieren (auf linux wie in install_jQuery.txt oder auf Windows gehts glaub ich einfacher)
--- flugbuchung_server.py
+++ flugbuchung_server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, json, request
 from flask.ext.mysql import MySQL
-from werkzeug import generate_password_hash, check_password_hash
+from Werkzeug import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -8,21 +8,31 @@ app = Flask(__name__)
 @app.route("/signUp")
 def sign_up():
     return render_template("sign_up.html")
+    # add server-db interaction code
 
 
 @app.route("/logIn")
 def log_in():
     return render_template("log_in.html")
+# add server-db interaction code
+    # read values
+    # validate values
 
 
 @app.route("/chooseDate")
 def choose_date():
     return render_template("choose_date.html")
+# add server-db interaction code
+    # read values
+    # validate values
 
 
 @app.route("/bookFlight")
 def book_flight():
     return render_template("book_flight.html")
+# add server-db interaction code
+    # read values
+    # validate values
 
 
 if __name__ == "__main__":
